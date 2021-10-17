31510f9e1b9f23db1527d5920750a89e4bffa7de
qtmtg66rrjwpndvn <qtmtg66rrjwpndvn@gmail.com>
2021-10-17 02:59:08 +0000

    add REPL support for user story 4, genBestMatchesTeams in progress

--- src/main/java/edu/brown/cs/student/main/command/RecSysGenGroupsCommand.java
+++ src/main/java/edu/brown/cs/student/main/command/RecSysGenGroupsCommand.java
@@ -1,8 +1,21 @@
 package edu.brown.cs.student.main.command;
 
+import edu.brown.cs.student.recsys.RecommendationSystem;
+
 public class RecSysGenGroupsCommand implements Command {
   @Override
   public void execute(String[] args) {
-
+    //make sure that the input is actually valid
+    if (args.length != 2) {
+      System.out.println(
+          "Error: Invalid number of arguments provided to the 'recsys_rec' command."
+              + "Your REPL input should follow the following format:"
+              + "recsys_rec <team_size>");
+    }
+    //parse teamSize argument
+    int teamSize = Integer.parseInt(args[1]);
+    //generate set of best matched teams for entire dataset
+    String message = RecommendationSystem.getInstance().genBestMatchedTeams(teamSize);
+    System.out.println(message);
   }
 }
--- src/main/java/edu/brown/cs/student/recsys/RecommendationSystem.java
+++ src/main/java/edu/brown/cs/student/recsys/RecommendationSystem.java
@@ -4,11 +4,14 @@ import edu.brown.cs.student.api.ApiAggregator;
 import edu.brown.cs.student.bloomfilter.BloomFilterRecommender;
 import edu.brown.cs.student.bloomfilter.recommender.Item;
 import edu.brown.cs.student.kdtree.KDTree;
+import edu.brown.cs.student.main.command.Command;
 import edu.brown.cs.student.orm.DataManager;
+import edu.brown.cs.student.orm.Users;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * RecommendationSystem is a proxy class that is used by the main REPL to simplify interactions
@@ -113,9 +116,92 @@ public class RecommendationSystem {
   }
 
   /**
-   * User Story 4: generate the set of best matched teams across the entire class
+   *
+   * <p>
+   * >
    */
-  public List<Object> genBestMatchedTeams() {
+  /**
+   * User Story 4: generate the set of best matched teams across the entire class
+   *
+   * @param teamSize - size of individual teams
+   * @return a String representation of the best matched teams for all students
+   * in the dataset. Return value has the following structure
+   * List<
+   * List<[group_1_student_1, group_1_student_2, … group_1_student_k]>
+   * List<[group_2_student_1, group_2_student_2, … group_2_student_k]>
+   * ...
+   * List<[group_n_student_1, group_n_student_2, … group_n_student_k]>
+   * >
+   */
+  public String genBestMatchedTeams(int teamSize) {
+    // list to store all students in dataset (returned from orm)
+    List<Object> AllStudents = null;
+
+    // hashmap of student to their recs
+    Map<Object, List<Object>> studentRecs = new HashMap<>();
+    // hashmap of student to their team members (duplicate students stored...)
+    // Map<Object, List<Object>> AllTeams = new HashMap<>();
+
+    // list to store all teams
+    List<List<Object>> AllTeams = null;
+
+    try {
+      // check if this selects all students, or if need to call custom query .sql("SELECT ...")
+      AllStudents = orm.select("", "", Object.class);
+
+      //  if the total number of students is not divisible by teamSize
+      //  return larger groups than desired in input teamSize
+      //  Why? Smaller teams might be overwhelmed by work
+      if (AllStudents.size() % teamSize != 0) {
+        teamSize++;
+      }
+      // generate recommendations for every student in the database
+      for (Object student : AllStudents) {
+        // get numeric recommendations
+        List<Object> numericRecommendations = kdTree.kNearestNeighbors(student, AllStudents.size());
+        // get categorical recommendations
+        List<Object> categoricalRecommendations =
+            bloomFilterRecommender.getTopKRecommendations((Item) student, AllStudents.size());
+        // combine recommendations
+        List<Object> AllRecsForStudent =
+            combineRecommendations(teamSize, numericRecommendations, categoricalRecommendations);
+
+        // add student (key) recs (value) to hashmap
+        studentRecs.put(student, AllRecsForStudent);
+      }
+
+      // loop through map and assign highest recommended students to this students team
+      for (Map.Entry<Object, List<Object>> student : studentRecs.entrySet()) {
+        int numTeamMates = teamSize;
+        List<Object> newTeam = new ArrayList();
+        for (List<Object> team : AllTeams) {
+          // if student already exists in another students team then move on to next rec
+          if (team.contains(student.getValue())) {
+
+          } else {
+            newTeam.add()
+            numTeamMates--;
+          }
+        }
+
+      }
+
+
+    } catch (Exception e) {
+      System.out.println("[Error: RecommendationSystem] genBestMatchedTeams(): Your select method "
+          + "returned an Exception.");
+    }
+
+    // check if student already exists in another team, if so, increment teamSize for next call to
+    // get more recommended students. Eventually will have all of them
+    // student A optimal for both students B and C
+    // prioritize students compatible with highest # of other students?
+    // what if total # of students not divisible by team size => return larger groups than desired.
+    // print results to REPL interface?
+
+    // handle missing columns?
+
+    // return serialized String version of results
     return null;
   }
 }
